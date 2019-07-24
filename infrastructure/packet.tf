38d55d3640a5fb19a09e992a6d19ff28ba7cb1e6
Matt Oswalt <matt@keepingitclassless.net>
2019-07-24 10:22:56 -0700

    Commenting out packet for now
    Signed-off-by: Matt Oswalt <matt@keepingitclassless.net>

--- infrastructure/packet.tf
+++ infrastructure/packet.tf
@@ -11,28 +11,28 @@ locals {
 }
 
 # Baby server for running our kubernetes master. Small, but very important :)
-resource "packet_device" "lily" {
-  hostname         = "lily"
-  plan             = "t1.small.x86"
-  facilities = ["sjc1"]
-  operating_system = "centos_7"
-  billing_cycle    = "hourly"
-  project_id       = "${var.packet_project_id}"
-  tags = ["antidote-master"]
-  depends_on       = ["packet_ssh_key.mierdinkey"]
-}
+# resource "packet_device" "lily" {
+#   hostname         = "lily"
+#   plan             = "t1.small.x86"
+#   facilities = ["sjc1"]
+#   operating_system = "centos_7"
+#   billing_cycle    = "hourly"
+#   project_id       = "${var.packet_project_id}"
+#   tags = ["antidote-master"]
+#   depends_on       = ["packet_ssh_key.mierdinkey"]
+# }
 
-resource "packet_device" "antidote-worker" {
-  count = 3
-  hostname         = "antidote-worker-${count.index}"
-  plan             = "c1.small.x86"
-  facilities = ["sjc1"]
-  operating_system = "centos_7"
-  billing_cycle    = "hourly"
-  project_id       = "${var.packet_project_id}"
-  tags = ["antidote-worker"]
-  depends_on       = ["packet_ssh_key.mierdinkey"]
-}
+# resource "packet_device" "antidote-worker" {
+#   count = 3
+#   hostname         = "antidote-worker-${count.index}"
+#   plan             = "c1.small.x86"
+#   facilities = ["sjc1"]
+#   operating_system = "centos_7"
+#   billing_cycle    = "hourly"
+#   project_id       = "${var.packet_project_id}"
+#   tags = ["antidote-worker"]
+#   depends_on       = ["packet_ssh_key.mierdinkey"]
+# }
 
 resource "packet_ssh_key" "mierdinkey" {
   name       = "mierdinkey"
